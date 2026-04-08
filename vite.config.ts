4df244fc422f0942a4fcdb943a099344c8a84162
jtrang <janice.trang@gmail.com>
2026-04-08 17:31:24 -0400

    Remove tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import spaServer from "vite-spa-server";
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     spaServer({
       entry: "./src/index.ts",
       port: 3000,
